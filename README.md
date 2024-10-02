05ad9043d35a9ad0dc32c8dbe5dfafc7e3402aa2
A.A. Suvorov <smartlegiondev@gmail.com>
2024-10-02 15:45:48 +0700

    Server Info Bot v0.0.2

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
 ## Use:
 
 - `export SERVER_INFO_BOT_TOKEN="<token>"`
-- `export SERVER_INFO_BOT_CHAT_ID="<chat id>"`
+- `export SERVER_INFO_BOT_CHAT_ID_1="<chat id>"`
+- `export SERVER_INFO_BOT_CHAT_ID_2="<chat id>"`
 - `. bot.sh`
--- bot.sh
+++ bot.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-sleep 300
+#sleep 300
 
 LOCAL_IP=$(ip addr show | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}' | cut -d'/' -f1)
 
 MESSAGE="Local IP: $LOCAL_IP"
 
-curl -s -X POST "https://api.telegram.org/bot$SERVER_INFO_BOT_TOKEN/sendMessage" -d "chat_id=$SERVER_INFO_BOT_CHAT_ID&text=$MESSAGE"
+curl -s -X POST "https://api.telegram.org/bot$SERVER_INFO_BOT_TOKEN/sendMessage" -d "chat_id=$SERVER_INFO_BOT_CHAT_ID_1&text=$MESSAGE"
+curl -s -X POST "https://api.telegram.org/bot$SERVER_INFO_BOT_TOKEN/sendMessage" -d "chat_id=$SERVER_INFO_BOT_CHAT_ID_2&text=$MESSAGE"
 
 echo "Сообщение отправлено: $MESSAGE"
